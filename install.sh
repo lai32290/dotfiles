58192eee61e7a5e8ab5b44bb44fc71e9cda0213f
Lai <lai32690@gmail.com>
2017-07-16 21:59:16 -0300

    add install file to create simbolic link

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s ${PWD}/tmux/.tmux.conf $HOME/
+ln -s ${PWD}/vim/.vimrc.local $HOME/
+ln -s ${PWD}/vim/.vimrc.local.bundles $HOME/
+ln -s ${PWD}/zsh/.zshrc $HOME/
+ln -s ${PWD}/zsh/.profile-zshrc $HOME/
