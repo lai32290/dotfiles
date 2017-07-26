b1044946083c135c7d79f0c18bc44dc22bc13520
Lai Xuancheng <lai32690@gmail.com>
2017-07-26 10:22:26 -0300

    add notify-osd configuration

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ ln -s ${PWD}/vim/.vimrc.local $HOME/
 ln -s ${PWD}/vim/.vimrc.local.bundles $HOME/
 ln -s ${PWD}/zsh/.zshrc $HOME/
 ln -s ${PWD}/zsh/.profile-zshrc $HOME/
+ln -s ${PWD}/notify-osd/.notify-osd $HOME/
