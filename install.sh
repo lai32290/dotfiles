550370dde069935d86a744eacde8f1895194d810
Lai <lai32690@gmail.com>
2017-08-15 07:40:58 -0300

    add UltiSnips

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ ln -s ${PWD}/vim/.vimrc.local.bundles $HOME/
 ln -s ${PWD}/zsh/.zshrc $HOME/
 ln -s ${PWD}/zsh/.profile-zshrc $HOME/
 ln -s ${PWD}/notify-osd/.notify-osd $HOME/
+ln -s ${PWD}/vim/UltiSnips $HOME/.vim/
